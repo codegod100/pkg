059950bec2c5a4ad6c052d003221d4050832e804
codegod100 <codegod100@users.noreply.github.com>
2026-08-16 20:11:39 -0700

    Clean existing Cellar directory before Buck2 install

--- src/pkg_ffi.erl
+++ src/pkg_ffi.erl
@@ -65,7 +65,7 @@ buck2_install(Workspace0, Target0, Name0, Version0, Binary0) ->
       Lines = string:lexemes(Output, "\n"), Paths = [P || L <- Lines, P <- output_path(L), filelib:is_file(filename:join(AbsWorkspace, P))],
       case Paths of
         [Source0|_] -> Source = filename:join(AbsWorkspace, Source0), Destination = filename:join([Cellar, "bin", Binary]),
-          ok = filelib:ensure_dir(filename:join(Destination, "x")), _ = file:delete(Destination),
+          ok = filelib:ensure_dir(filename:join(Destination, "x")), _ = file:del_dir_r(Destination),
           {ok, _} = file:copy(Source, Destination), ok = file:change_mode(Destination, 8#755), ok = filelib:ensure_dir(filename:join(Bin, "x")),
           _ = file:delete(filename:join(Bin, Binary)), ok = file:make_symlink(Destination, filename:join(Bin, Binary)),
           {ok, unicode:characters_to_binary(filename:join(Bin, Binary))};
