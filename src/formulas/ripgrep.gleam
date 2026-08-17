bdc9198ad19461bd86b4426e85d6fbd5dc7c6da7
codegod100 <codegod100@users.noreply.github.com>
2026-08-16 18:36:00 -0700

    Implement verified prebuilt formula installation

--- src/formulas/ripgrep.gleam
+++ src/formulas/ripgrep.gleam
@@ -6,7 +6,10 @@ pub fn formula() -> formula.Formula {
     "14.1.0",
     "Fast recursive search tool",
     "https://github.com/BurntSushi/ripgrep",
-    formula.Prebuilt("https://github.com/BurntSushi/ripgrep/releases", ""),
+    formula.Prebuilt(
+      "https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep-14.1.0-x86_64-unknown-linux-musl.tar.gz",
+      "f84757b07f425fe5cf11d87df6644691c644a5cd2348a2c670894272999d3ba7",
+    ),
     ["rg"],
     [],
   )
--- src/pkg_ffi.erl
+++ src/pkg_ffi.erl
@@ -1,7 +1,8 @@
 -module(pkg_ffi).
 -export([install/4]).
-install(Url, Name, Version, Binary) ->
-  Home = os:getenv("HOME"),
+install(Url0, Name0, Version0, Binary0) ->
+  Url = unicode:characters_to_list(Url0), Name = unicode:characters_to_list(Name0), Version = unicode:characters_to_list(Version0), Binary = unicode:characters_to_list(Binary0),
+  Home = case os:getenv("HOME") of false -> "."; Value -> Value end,
   Root = filename:join([Home, ".local", "share", "pkg"]),
   Cellar = filename:join([Root, "Cellar", Name, Version]),
   Bin = filename:join([Home, ".local", "bin"]),
@@ -9,9 +10,9 @@ install(Url, Name, Version, Binary) ->
   ok = filelib:ensure_dir(filename:join(Cellar, "x")),
   ok = filelib:ensure_dir(filename:join(Bin, "x")),
   Cmd = "curl --fail --location --silent --show-error --proto '=https' --tlsv1.2 -o " ++ q(Tmp) ++ " " ++ q(Url) ++
-        " && tar -xzf " ++ q(Tmp) ++ " -C " ++ q(Cellar) ++ " --strip-components=1 && ln -sfn " ++ q(filename:join([Cellar, "bin", Binary])) ++ " " ++ q(filename:join(Bin, Binary)) ++ " && rm -f " ++ q(Tmp),
+        " && tar -xzf " ++ q(Tmp) ++ " -C " ++ q(Cellar) ++ " --strip-components=1 && ln -sfn " ++ q(filename:join(Cellar, Binary)) ++ " " ++ q(filename:join(Bin, Binary)) ++ " && rm -f " ++ q(Tmp),
   case os:cmd(Cmd) of
-    [] -> {ok, filename:join(Bin, Binary)};
+    [] -> {ok, unicode:characters_to_binary(filename:join(Bin, Binary))};
     Output -> {error, Output}
   end.
 q(S) -> "'" ++ string:replace(S, "'", "'\"'\"'", all) ++ "'".
